81d338783e3c59ab5ac0b997620eb62a61589e82
Tiago Soares de Almeida <tiago.almeidat@outlook.com>
2025-03-14 21:36:41 -0300

    Dockerfile frontend refaturando

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -5,7 +5,7 @@ function Navbar() {
     <header>
         <nav className="container">
             <div className="dsmovie-nav-content">
-                <h1>TSMovie v2</h1>
+                <h1>Movie v2</h1>
                 <a href="https://github.com/tiagotsa">
                     <div className="dsmovie-contact-container">
                         <GithubIcon />
