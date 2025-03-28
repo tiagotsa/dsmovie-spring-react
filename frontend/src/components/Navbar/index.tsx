758f2e51734dd7d5b2e2f1d5ed7a3d0bbff4ba96
Tiago Soares de Almeida <tiago.almeidat@outlook.com>
2025-03-28 04:24:00 -0300

    Repaturando frontend

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -5,7 +5,7 @@ function Navbar() {
     <header>
         <nav className="container">
             <div className="dsmovie-nav-content">
-                <h1>TSSAMovie</h1>
+                <h1>TMovie</h1>
                 <a href="https://github.com/tiagotsa">
                     <div className="dsmovie-contact-container">
                         <GithubIcon />
