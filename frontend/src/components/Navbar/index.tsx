b7d07b518bce166d2a818d5f673bfad77a69371b
Tiago Soares de Almeida <tiago.almeidat@outlook.com>
2025-03-28 21:00:47 -0300

    refaturamento

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,25 +1,22 @@
 import { ReactComponent as GithubIcon } from 'assets/img/github.svg';
 import './styles.css'
+
 function Navbar() {
-     return (
-    <header>
-        <nav className="container">
-            <div className="dsmovie-nav-content">
-<<<<<<< HEAD
-                <h1>TSAMovie</h1>
-=======
-                <h1>TASMovie</h1>
->>>>>>> 6420299264631a6f017ea50cb0d00ffab18701ba
-                <a href="https://github.com/tiagotsa">
-                    <div className="dsmovie-contact-container">
-                        <GithubIcon />
-                        <p className="dsmovie-contact-link">/tiagotsa</p>
-                    </div>
-                </a>
-            </div>
-        </nav>
-    </header>
-  );
+    return (
+        <header>
+            <nav className="container">
+                <div className="dsmovie-nav-content">
+                    <h1>TSqwMovie</h1>
+                    <a href="https://github.com/tiagotsa">
+                        <div className="dsmovie-contact-container">
+                            <GithubIcon />
+                            <p className="dsmovie-contact-link">/tiagotsa</p>
+                        </div>
+                    </a>
+                </div>
+            </nav>
+        </header>
+    );
 }
 
 export default Navbar;
