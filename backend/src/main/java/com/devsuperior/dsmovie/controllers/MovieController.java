164c695ac128f8b3238559d13d5eb7dcb0be543e
Tiago Soares de Almeida <tiago.almeidat@outlook.com>
2025-04-15 04:24:58 -0300

    refaturar

--- backend/src/main/java/com/devsuperior/dsmovie/controllers/MovieController.java
+++ backend/src/main/java/com/devsuperior/dsmovie/controllers/MovieController.java
@@ -12,7 +12,7 @@ import com.devsuperior.dsmovie.services.MovieService;
 
 
 @RestController
-@RequestMapping(value = "/movies")
+@RequestMapping(value = "/api/movies")
 public class MovieController {
 	
 	 @Autowired
