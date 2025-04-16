fff508c54368fa74c28391242dc400cb710d093d
Tiago Soares de Almeida <tiago.almeidat@outlook.com>
2025-04-16 02:05:01 -0300

    refaturar

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,1 +1,2 @@
-export const BASE_URL = process.env.REACT_APP_API_URL ?? "http://localhost:8080"
+// Use '/api' como fallback, que funciona com o proxy Nginx
+export const BASE_URL = process.env.REACT_APP_API_URL ?? "/api";
